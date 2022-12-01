6c33e97818920e093bd11b0be99032a96a3924bd
Jakub Lecki <lec.jakub@gmail.com>
2022-12-01 22:06:42 +0100

    nrf: Init workspace for three device configuration.
    Configurations are:
      * Master
      * Client
      * Jammer
    
    Use Makefiles for building, flashing, getting logs and cleaning.

---
+++ nrf/common/nrf.mk
@@ -0,0 +1,17 @@
+build:
+	$(MAKE) -C pca10059/s140/armgcc
+
+flash: build
+	$(info $(board_id))
+	sshpass -p $(remote_pass) scp -P 120$(board_id) pca10059/s140/armgcc/_build/nrf52840_xxaa.hex $(sdk_path)/components/softdevice/s140/hex/s140_nrf52_7.0.1_softdevice.hex $(remote_user)@153.19.49.102:/var/tmp
+	sshpass -p $(remote_pass) ssh -p 120$(board_id) $(remote_user)@153.19.49.102 ' \
+		   curl http://153.19.49.102:3001/resetNRF52v2/$(board_id) \
+		&& nrfutil pkg generate --hw-version 52 --sd-req 0x00 --sd-id 0xCA --softdevice /var/tmp/s140_nrf52_7.0.1_softdevice.hex  --debug-mode --application /var/tmp/nrf52840_xxaa.hex /var/tmp/dfu.zip \
+		&& nrfutil dfu usb-serial -pkg /var/tmp/dfu.zip -p $(usb_path) -b 115200 \
+		&& rm /var/tmp/*.hex'
+
+log:
+	sshpass -p $(remote_pass) ssh -p 120$(board_id) $(remote_user)@153.19.49.102 -t 'minicom -D $(usb_path)'
+
+clean:
+	$(MAKE) clean -C pca10059/s140/armgcc
